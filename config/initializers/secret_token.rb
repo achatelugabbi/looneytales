f7774edde470ece143a866f7f3020a63166d0f5d
Rakesh Ramineni <raxramni@gmail.com>
2012-03-09 19:57:31 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Looneytales::Application.config.secret_token = '0977fa230e56ae022c7c432fc2317cdae93c5194364d5767f7b70f092c1585702e40d7e7594ddb6f55825d5419f0edc132a513fc1a688d6d27d9e6a8f6f249f8'
