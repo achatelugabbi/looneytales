3d75018ef69f54897660b5b20fe784ae942ecc86
Rakesh Ramineni <raxramni@gmail.com>
2012-03-24 19:59:44 -0700

    added masonry

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+//= require core
+//= require vendor/jquery.masonry.min
---
+++ app/assets/javascripts/core.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+  $('#disp-area').masonry({
+    itemSelector: '.disp-unit'
+  });
+});
