a366866cb1e334fd1b5cf86d2e511f4e7bd1ea1c
Rakesh Ramineni <raxramni@gmail.com>
2012-03-28 21:44:03 -0700

    improved frontend

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all.to_a
     k = []
-    20.times { @movies.each {|movie| k<< movie}}
+    60.times { @movies.each {|movie| k<< movie}}
     @movies = k
   end
 
