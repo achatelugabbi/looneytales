76abb1ce13fee2d988219763dc166796c233cd72
Rakesh Ramineni <raxramni@gmail.com>
2012-03-28 21:25:13 -0700

    improved frontend

--- app/assets/javascripts/core.js
+++ app/assets/javascripts/core.js
@@ -2,7 +2,8 @@ $(document).ready(function() {
   var $container = $('#disp-area');
   $container.imagesLoaded(function(){
     $container.masonry({
-      itemSelector: '.disp-unit'
+      itemSelector: '.disp-unit',
+      isFitWidth: true
     });
    });
 });
