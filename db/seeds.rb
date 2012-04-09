6c96e0670b5b9e0a49ea8523267684fb1f80017a
Rakesh Ramineni <raxramni@gmail.com>
2012-04-09 01:39:42 -0700

    changed seeding to json

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,13 +4,37 @@
 # Examples:
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#   Mayor.create(name: 
 
-if Rails.env = "development"
+#require 'yaml'
+require 'json'
+
+if Rails.env == "development"
   Video.delete_all
   Movie.delete_all
 end
+puts "loading videos json file"
+#@videos = YAML::load(File.open("#{Rails.root}/samples/videos.yml"))
+videos_json_file = File.read "#{Rails.root}/samples/videos.json"
+videos_json = JSON.parse(videos_json_file)
 
+videos_json.each do |v| 
+  Video.safely.create!(v)
+  puts "created #{v["title"]}"
+end
+
+puts "loading videos json file"
+#@movies = YAML::load(File.open("#{Rails.root}/samples/videos.yml"))
+movies_json_file = File.read "#{Rails.root}/samples/movies.json"
+movies_json = JSON.parse(movies_json_file)
+
+
+movies_json.each do |m| 
+  Movie.safely.create!(m)
+  puts "created #{m["title"]}"
+end
+
+=begin
 swarna_kamalam_video = Video.new(link: 'https://www.youtube.com/v/BsGhE116zLQ', title: 'Swarna Kamalam', language: 2, host: 'youtube', duration: 8571, is_movie: true, genre: 3)
 swarna_kamalam_video.keywords = ['swarna kamalam', 'swarnakamalam', 'swarna kamalam movie', 'venkatesh in swarna kamalam', 'bhanu priya', 'k.viswanadh movies', 'classical movies', 'classic movies', 'swarnakamalam songs', 'telugu movie songs', 'teluguone movies', 'free telugu movies', 'free hd movies', 'high definiton movies', 'swarna kamalam video songs']
 swarna_kamalam_video.thumbnails = [{:url=> 'http://i.ytimg.com/vi/BsGhE116zLQ/0.jpg', :height=>360, :width=>480}]
@@ -22,3 +46,4 @@ swarna_kamalam_movie = Movie.new(name: 'Swarna Kamalam', video_ids: [swarna_kama
 swarna_kamalam_movie.cast = ['Venkatesh', 'Bhanupriya', 'Shanmukha Srinivas', 'Mucherla Aruna', 'Sakshi Ranga Rao', 'Sharon Loven', 'Dubbing Janaki', 'Pavala SyAmala']
 swarna_kamalam_movie.desc = "Chandra, an artist meets Meena, who is her neighbour. As her father is a dance master Chandra urges Meena to learn dance, but Meena is not interested. He even arranges a dance programme for Meena who behaves mischeviously on the stage."
 swarna_kamalam_movie.safely.save!
+=end
